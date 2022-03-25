bbee809a4c3aef27fbcc685bf63b8f80061937f1
Alexander Ilinykh <ilinykh_ab@sima-land.ru>
2022-03-25 18:05:37 +0500

    #22-created-base-structure

---
+++ src/components/app/index.js
@@ -0,0 +1,12 @@
+import styles from "./app.module.scss";
+
+function App() {
+  return (
+    <div className={styles.app}>
+      <p>Компонент App</p>
+    </div>
+    //Лишний комментарий
+  );
+}
+
+export default App;
