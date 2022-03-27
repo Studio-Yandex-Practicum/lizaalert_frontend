ae24e7b8a5117ca09a60562211d589fccbf1a9da
Nik68rus <nik200x@mail.ru>
2022-03-27 11:57:49 +0300

    #31 - card component styles

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,7 +1,13 @@
+import Card from '../templates/card/card';
 import appStyles from './app.module.scss';
 
 function App() {
-  return <div className={appStyles.wrapper}>hello word)</div>;
+  return (
+    <div className={appStyles.wrapper}>
+      <Card>hello word)</Card>
+      <Card nopadding>hello word)</Card>
+    </div>
+  );
 }
 
 export default App;
--- src/components/templates/card/card.js
+++ src/components/templates/card/card.js
@@ -1,11 +1,27 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import styles from './card.module.scss';
 
-const Card = (props) => {
+function Card({ children, nopadding }) {
   return (
-    <div>
-      {props.children}
+    <div
+      className={`${styles.card}${nopadding ? ` ${styles.cardNoPadding}` : ''}`}
+    >
+      {children}
     </div>
   );
 }
+
+Card.defaultProps = {
+  nopadding: false,
+};
+
+Card.propTypes = {
+  children: PropTypes.oneOfType([
+    PropTypes.arrayOf(PropTypes.node),
+    PropTypes.node,
+  ]).isRequired,
+  nopadding: PropTypes.bool,
+};
 
 export default Card;
