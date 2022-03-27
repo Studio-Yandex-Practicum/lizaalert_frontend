0960557593938d7389ed46eed679ec5227128aae
Nik68rus <nik200x@mail.ru>
2022-03-27 12:33:18 +0300

    #31 switch js to jsx

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,13 +1,7 @@
-import Card from '../templates/card/card';
 import appStyles from './app.module.scss';
 
 function App() {
-  return (
-    <div className={appStyles.wrapper}>
-      <Card>hello word)</Card>
-      <Card nopadding>hello word)</Card>
-    </div>
-  );
+  return <div className={appStyles.wrapper}>hello word)</div>;
 }
 
 export default App;
