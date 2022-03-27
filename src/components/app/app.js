dc3190b112556a2b68e2016f4e67c4740458486f
Yuri Zhulev <yuri.zhulyov@gmail.com>
2022-03-27 14:34:05 +0300

    clear app.js

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,13 +1,7 @@
 import appStyles from './app.module.scss';
-import Header from '../templates/header/header';
 
 function App() {
-  return (
-    <div className={appStyles.wrapper}>
-      <Header />
-      hello word)
-    </div>
-  );
+  return <div className={appStyles.wrapper}>hello word)</div>;
 }
 
 export default App;
