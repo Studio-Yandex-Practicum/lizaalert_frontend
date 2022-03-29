7d3dd5221fe9ce990b9fdbbd7b880d7974238ba6
MigelG <za4elovek@yandex.ru>
2022-03-29 08:00:37 +0300

    #5-review-button-component

--- src/components/molecules/button/button.jsx
+++ src/components/molecules/button/button.jsx
@@ -2,51 +2,58 @@ import PropTypes from 'prop-types';
 import styles from './button.module.scss';
 import Icon from './icon';
 
-const Button = function ({ children, view, icon, onClick, ...props }) {
-  let buttonClasses = styles.button;
-  switch (view) {
-    case 'primary': {
-      buttonClasses += ` ${styles.primary}`;
-      break;
-    }
-    case 'secondary': {
-      buttonClasses += ` ${styles.secondary}`;
-      break;
-    }
-    case 'text': {
-      buttonClasses += ` ${styles.text}`;
-      break;
-    }
-    default:
-      buttonClasses += ` ${styles.primary}`;
-  }
+const btnView = {
+  primary: ` ${styles.primary}`,
+  secondary: ` ${styles.secondary}`,
+  text: ` ${styles.text}`,
+};
+
+const Button = function ({
+  children,
+  view,
+  icon,
+  iconPosition,
+  onClick,
+  className,
+  disabled,
+  type,
+}) {
+  const btnClasses = styles.button + (btnView[view] ?? btnView.primary);
 
   return (
     <button
       onClick={onClick}
-      type="button"
-      {...props}
-      className={buttonClasses}
+      className={`${btnClasses}${className && ` ${className}`}`}
+      disabled={disabled}
+      type={type === 'submit' ? 'submit' : 'button'}
     >
-      {icon === 'back' ? <Icon id="back" /> : null}
+      {iconPosition === 'back' && <Icon icon={icon} id="back" />}
       {children}
-      {icon === 'forward' ? <Icon id="forward" /> : null}
+      {iconPosition === 'forward' && <Icon icon={icon} id="forward" />}
     </button>
   );
 };
 
 Button.propTypes = {
   children: PropTypes.string,
-  view: PropTypes.string,
+  view: PropTypes.oneOf(['primary', 'secondary', 'text']),
   icon: PropTypes.string,
+  iconPosition: PropTypes.oneOf(['back', 'forward']),
   onClick: PropTypes.func,
+  className: PropTypes.string,
+  disabled: PropTypes.bool,
+  type: PropTypes.oneOf(['button', 'submit']),
 };
 
 Button.defaultProps = {
   children: '',
   view: 'primary',
   icon: '',
+  iconPosition: '',
   onClick: () => {},
+  className: '',
+  disabled: false,
+  type: 'button',
 };
 
 export default Button;
