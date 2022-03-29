20ec2849625866fe609f58cddf61f5140eb20d83
Yuri Zhulev <yuri.zhulyov@gmail.com>
2022-03-29 12:15:21 +0300

    #53 implement Header Links in Header, add active class names, scss refactor

--- src/components/templates/header/header.jsx
+++ src/components/templates/header/header.jsx
@@ -1,13 +1,33 @@
+import { useLocation } from 'react-router-dom';
+import HeaderLink from '../../molecules/header-link/header-link';
 import styles from './header.module.scss';
 
 function Header() {
+  const location = useLocation();
+
+  const { pathname } = location;
+
+  const stplitLocation = pathname.split('/');
+
   return (
     <header className={styles.header}>
       <div className={styles.headerContainer}>
         <h1 className={styles.headerText}>Учебная платформа</h1>
         <ul className={styles.headerLinks}>
-          <li>HeaderLink</li>
-          <li>HeaderLink</li>
+          <li>
+            <HeaderLink
+              text="Курсы"
+              link="/"
+              isActive={stplitLocation[1] === '' && true}
+            />
+          </li>
+          <li>
+            <HeaderLink
+              text="Профиль"
+              link="/profile"
+              isActive={stplitLocation[1] === 'profile' && true}
+            />
+          </li>
         </ul>
       </div>
     </header>
