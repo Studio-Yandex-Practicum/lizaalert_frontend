a73a63b4c8826da864a01bfdeaf6605be5182114
Alyona Irisova <elaineirr.910@gmail.com>
2022-03-30 19:02:01 +0400

    #9-add-import-to-molecules-index

--- src/components/molecules/index.js
+++ src/components/molecules/index.js
@@ -1,6 +1,6 @@
 import Button from './button/button';
 import TextWithIcon from './text-with-icon/text-with-icon';
 import ProfileMeta from './profile-meta/profile-meta';
+import Checkbox from './checkbox/checkbox';
 
-export { TextWithIcon, Button, ProfileMeta };
-
+export { TextWithIcon, Button, ProfileMeta, Checkbox };
