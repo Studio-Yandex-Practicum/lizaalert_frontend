370a0553453e61e5811a6029b62c0b516cd45a38
Yuri Zhulev <yuri.zhulyov@gmail.com>
2022-03-30 09:52:42 +0300

    #53 fix isActive check for HeaderLinks

--- src/components/templates/header/header.jsx
+++ src/components/templates/header/header.jsx
@@ -1,32 +1,37 @@
-import { useLocation } from 'react-router-dom';
+import { useLocation, NavLink } from 'react-router-dom';
 import HeaderLink from '../../molecules/header-link/header-link';
 import routes from '../../../config/routes';
 import styles from './header.module.scss';
 
 function Header() {
   const location = useLocation();
-
   const { pathname } = location;
-
   const splitLocation = pathname.split('/');
 
   return (
     <header className={styles.header}>
       <div className={styles.headerContainer}>
-        <h1 className={styles.headerText}>Учебная платформа</h1>
+        <NavLink to={routes.courses.path} className={styles.headerTextLink}>
+          <h1 className={styles.headerText}>Учебная платформа</h1>
+        </NavLink>
         <ul className={styles.headerLinks}>
           <li>
             <HeaderLink
               text={routes.courses.title}
               link={routes.courses.path}
-              isActive={splitLocation[1] === '' && true}
+              // Если route для курсов изменится, можно будет удалить splitLocation[1] === ''
+              isActive={
+                (splitLocation[1] === routes.courses.path ||
+                  splitLocation[1] === '') &&
+                true
+              }
             />
           </li>
           <li>
             <HeaderLink
               text={routes.profile.title}
               link={routes.profile.path}
-              isActive={splitLocation[1] === 'profile' && true}
+              isActive={splitLocation[1] === routes.profile.path && true}
             />
           </li>
         </ul>
