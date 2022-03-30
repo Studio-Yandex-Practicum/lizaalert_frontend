8ce289e29857cf9a4fec1dea35358f690613e2da
Maksim Tropnikov <makstropnikov@gmail.com>
2022-03-31 01:57:10 +0700

    #48 fix remarks after review: added form, uncomment

--- src/components/organisms/account-data/account-data.jsx
+++ src/components/organisms/account-data/account-data.jsx
@@ -1,32 +1,36 @@
 import PropTypes from 'prop-types';
-// import { useState } from 'react';
+import { useState } from 'react';
 import Card from '../../templates/card/card';
 import styles from './account-data.module.scss';
 
 function AccountData({ userPhoneNumber, userEmail, userPassword }) {
-  // const [isInputsEdited, setIsInputsEdited] = useState(false);
-  // const [userData, setUserData] = useState({
-  //   phoneNumber: userPhoneNumber,
-  //   email: userEmail,
-  //   password: userPassword,
-  // });
+  const [isInputsEdited, setIsInputsEdited] = useState(false);
+  const [inputValues, setInputValues] = useState({
+    phoneNumber: userPhoneNumber,
+    email: userEmail,
+    password: userPassword,
+  });
 
-  // const handleChange = (evt) => {
-  //   const { name, value } = evt.target;
-  //   setUserData({
-  //     ...userData,
-  //     [name]: value,
-  //   });
-  //   setIsInputsEdited(true);
-  // };
+  const onInputValuesChange = (evt) => {
+    setInputValues({
+      ...inputValues,
+      [evt.target.name]: evt.target.value,
+    });
+    if (!isInputsEdited) {
+      setIsInputsEdited(true);
+    }
+  };
 
   return (
     <Card className={styles.accountData}>
       <h2 className={styles.title}>Аккаунт</h2>
-      <div className={styles.inputSection}>{userPhoneNumber}</div>
-      <div className={styles.inputSection}>{userEmail}</div>
-      <div className={styles.inputSection}>{userPassword}</div>
-      <div className={styles.submitButton}>Сохранить изменения</div>
+
+      <form name="accountData" className={styles.form}>
+        <div className={styles.inputSection}>{userPhoneNumber}</div>
+        <div className={styles.inputSection}>{userEmail}</div>
+        <div className={styles.inputSection}>{userPassword}</div>
+        <div className={styles.submitButton}>Сохранить изменения</div>
+      </form>
     </Card>
   );
 }
