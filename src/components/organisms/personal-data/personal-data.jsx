feb57e2b5b20aedaac33752014fec05e372ecbf8
Ekaterina Yadrikhinskaya <ya78087@yandex.ru>
2022-03-31 20:50:30 +0300

    #47-refactor-little-change

--- src/components/organisms/personal-data/personal-data.jsx
+++ src/components/organisms/personal-data/personal-data.jsx
@@ -3,13 +3,7 @@ import { useState } from 'react';
 import Card from '../../templates/card/card';
 import styles from './personal-data.module.scss';
 
-function PersonalData({
-  userName,
-  userDateOfBirth,
-  userRegion,
-  userNickname,
-  userAvatar,
-}) {
+function PersonalData({ userName, userDateOfBirth, userRegion, userNickname }) {
   // изменение с false на true при внесении изменений в данные
   const [isInputChanged, setIsInputChanged] = useState(false);
   const [inputsValues, setInputsValues] = useState({
@@ -17,12 +11,12 @@ function PersonalData({
     dateOfBirth: userDateOfBirth,
     region: userRegion,
     nickname: userNickname,
-    avatar: userAvatar,
+    avatar: '',
   });
 
   // функция на событие onChange для инпутов
   const onInputValuesChange = (e) => {
-    if (e.target.value !== inputsValues[e.target.name]) {
+    if (e.target.value !== inputsValues[e.target.name] && isInputChanged) {
       setIsInputChanged(true);
       setInputsValues({ ...inputsValues, [e.target.name]: e.target.value });
     }
@@ -31,14 +25,12 @@ function PersonalData({
   return (
     <Card className={styles.personalData}>
       <h2 className={styles.title}>Личные данные</h2>
-
       <form name="personalData" className={styles.form}>
         <div className={styles.inputSection}>{userName}</div>
         <div className={styles.inputSection}>{userDateOfBirth}</div>
         <div className={styles.inputSection}>{userRegion}</div>
         <div className={styles.inputSection}>{userNickname}</div>
-        <div className={styles.inputSection}>{userAvatar}</div>
-
+        <div className={styles.inputSection} />
         {/* Кнопку потом использовать с disabled={!isInputChanged} */}
         <div className={styles.submitButton}>Сохранить изменения</div>
       </form>
@@ -51,7 +43,6 @@ PersonalData.defaultProps = {
   userDateOfBirth: '01.01.1990',
   userRegion: 'г. Санкт-Петербург',
   userNickname: 'Белка',
-  userAvatar: '',
 };
 
 PersonalData.propTypes = {
@@ -59,7 +50,6 @@ PersonalData.propTypes = {
   userDateOfBirth: PropTypes.string,
   userRegion: PropTypes.string,
   userNickname: PropTypes.string,
-  userAvatar: PropTypes.string,
 };
 
 export default PersonalData;
