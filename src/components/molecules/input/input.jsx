55f2a78746699ed062826bb7690b3bfcf27b64c4
Tatiana Roslyakova <tat-rs@yandex.ru>
2022-04-02 17:10:51 +0300

    #81 added input without labelName

--- src/components/molecules/input/input.jsx
+++ src/components/molecules/input/input.jsx
@@ -2,14 +2,14 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import classNames from 'classnames';
 
-import styles from './input.module.scss';
-
 import { Icon } from '../../atoms';
+
+import styles from './input.module.scss';
 
 /**
  * @description Компонент инпут с основной стилизацией (активное и неактивное состояние).
  *
- * - labelName - string - заголовок инпута
+ * - labelName - string - заголовок инпута, необязательный prop, не должен быть пустой строкой
  * - type - string - тип инпута
  * - inputName - string - имя инпута и id
  * - value - string - значение инпута
@@ -24,18 +24,22 @@ function Input({ labelName, type, inputName, value, onChange, accept }) {
     inputRef.current.focus();
   }
 
+  console.log(labelName);
+
   return (
     <div className={styles.container}>
       <label
         htmlFor={inputName}
-        className={classNames(styles.label, {
+        className={classNames({
+          [styles.label]: labelName,
           [styles.label_flex]: type === 'file',
         })}
       >
         {labelName}
-        {type === 'file' ? (
+        {labelName && type === 'file' ? (
           <Icon type="attachment" className={styles.icon} />
         ) : (
+          labelName &&
           type !== 'tel' && (
             <Icon type="edit" className={styles.icon} onClick={() => focus()} />
           )
@@ -61,10 +65,11 @@ function Input({ labelName, type, inputName, value, onChange, accept }) {
 
 Input.defaultProps = {
   accept: null,
+  labelName: null,
 };
 
 Input.propTypes = {
-  labelName: PropTypes.string.isRequired,
+  labelName: PropTypes.string,
   type: PropTypes.oneOf(['text', 'date', 'file', 'tel', 'email', 'password'])
     .isRequired,
   inputName: PropTypes.string.isRequired,
