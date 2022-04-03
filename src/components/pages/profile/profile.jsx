a0c739a8759a7869e07af6f1d4dd0ce2d59d208c
Yuri Zhulev <yuri.zhulyov@gmail.com>
2022-04-03 12:43:32 +0300

    #80 add layout and styling to profile page.

--- src/components/pages/profile/profile.jsx
+++ src/components/pages/profile/profile.jsx
@@ -1,12 +1,62 @@
+import { useLocation } from 'react-router-dom';
 import AccountData from '../../organisms/account-data/account-data';
 import AccountOverview from '../../organisms/account-overview/account-overview';
+import PersonalData from '../../organisms/personal-data/personal-data';
+import routes from '../../../config/routes';
+import styles from './profile.module.scss';
 
 function Profile() {
+  const location = useLocation();
+  const { pathname } = location;
+  const splitLocation = pathname.split('/');
+
   return (
-    <div>
-      <p>Profile Page</p>
-      <AccountOverview />
-      <AccountData />
+    <div className={styles.container}>
+      <h1>{routes[splitLocation[1]].title}</h1>
+      <div className={styles.content}>
+        <aside>
+          <AccountOverview />
+        </aside>
+        <main className={styles.main}>
+          <section className={styles.section}>
+            <PersonalData />
+            <div className={styles.descriprion}>
+              <p>
+                Просьба указывать настоящие ФИО, это нужно для получения
+                сертификатов
+              </p>
+              <p>
+                Географический регион влияет на доступность некоторых курсов и
+                занятий
+              </p>
+              <p>
+                Если вы не хотите, чтобы другие пользователи видели ваши ФИО,
+                укажите позывной на форуме — он будет отображаться вместо ваших
+                настоящих данных
+              </p>
+              <p>Требования к загружаемому фото:</p>
+              <ul>
+                <li>разрешение не менее 640x640px</li>
+                <li>размер не более 3 МБ</li>
+              </ul>
+            </div>
+          </section>
+          <section className={styles.section}>
+            <AccountData />
+            <div className={styles.descriprion}>
+              <p>
+                Номер телефона используется как уникальный идентификатор
+                пользователя, поэтому его нельзя поменять вручную
+              </p>
+              <p>
+                Если по какой-то причине у вас нет доступа к номеру телефона на
+                который вы регистрировали аккаунт, напишите в поддержку
+              </p>
+              <p>Email нужен для восстановления пароля</p>
+            </div>
+          </section>
+        </main>
+      </div>
     </div>
   );
 }
