c095c82d601a1b4dafc90e626a526a812b2b8c57
Nik68rus <nik200x@mail.ru>
2022-04-06 13:46:38 +0300

    #43 - fixed accordion inside accordion behaviour

--- src/components/templates/accordion/accordion.jsx
+++ src/components/templates/accordion/accordion.jsx
@@ -11,28 +11,56 @@ import styles from './accordion.module.scss';
  * - children - содержимое, которое будет скрываться (JSX или Компонент)
  * - button - "icon" | "text" - необязательный проп, отвечающий за кнопку в правом верхнем углу в виде иконки либо текста "Развернуть"/"Свернуть". По умолчанию равен "icon", если необходимо, чтобы отображался текст - добавьте проп button="text"
  * - className - string - необязательный проп - дополнительный css класс для стилизации ручки аккордеона через вложенность или задания внешних отступов (CSS-селектор: .classname > button {...})
+ * - open - boolean - начальное состояние аккордеона при рендере. По умолчанию false - аккордеон закрыт
  */
 
-function Accordion({ children, className, title, button }) {
-  const [isOpen, setIsOpen] = useState(false);
-  const [height, setHeight] = useState('0px');
+function Accordion({ children, className, title, button, open }) {
+  const [isOpen, setIsOpen] = useState(open);
+  const [height, setHeight] = useState(open ? 'auto' : '0px');
 
   const contentRef = useRef(null);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setHeight(isOpen ? `${contentRef.current.scrollHeight}px` : '0px');
+    }, 300);
+  }, [contentRef.current]);
 
   const updateContentHeight = () => {
     setHeight(isOpen ? `${contentRef.current.scrollHeight}px` : '0px');
   };
 
+  const innerAccordionToggleHandler = (evt) => {
+    if (evt.target !== contentRef.current) {
+      setHeight(
+        isOpen
+          ? `${contentRef.current.scrollHeight + evt.target.scrollHeight}px`
+          : '0px'
+      );
+    }
+  };
+
   useEffect(() => {
     window.addEventListener('resize', updateContentHeight);
+    contentRef.current.addEventListener(
+      'accordionToggle',
+      innerAccordionToggleHandler
+    );
     return () => {
       window.removeEventListener('resize', updateContentHeight);
+      contentRef.current.removeEventListener(
+        'accordionToggle',
+        innerAccordionToggleHandler
+      );
     };
   }, [updateContentHeight]);
 
   const toggleAccordion = () => {
     setIsOpen(!isOpen);
     setHeight(isOpen ? '0px' : `${contentRef.current.scrollHeight}px`);
+
+    const event = new Event('accordionToggle', { bubbles: true });
+    contentRef.current.dispatchEvent(event);
   };
 
   const renderButton = (type) => (
@@ -71,12 +99,14 @@ function Accordion({ children, className, title, button }) {
 Accordion.defaultProps = {
   className: '',
   button: 'icon',
+  open: false,
 };
 
 Accordion.propTypes = {
   className: PropTypes.string,
   title: PropTypes.string.isRequired,
   button: PropTypes.oneOf(['text', 'icon']),
+  open: PropTypes.bool,
   children: PropTypes.oneOfType([
     PropTypes.arrayOf(PropTypes.node),
     PropTypes.node,
