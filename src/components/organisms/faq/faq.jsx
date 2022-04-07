4553e7e0064c427d7209ecee8e7144e3ebc11277
Tatiana Roslyakova <tat-rs@yandex.ru>
2022-04-07 05:13:56 +0300

    #92 refactor FAQ

--- src/components/organisms/faq/faq.jsx
+++ src/components/organisms/faq/faq.jsx
@@ -1,65 +1,50 @@
 import React from 'react';
-import PropTypes from 'prop-types';
-import classNames from 'classnames';
 
 import Accordion from '../../templates/accordion/accordion';
 import Card from '../../templates/card/card';
 
 import styles from './faq.module.scss';
 
-function FAQ({ data }) {
-  const [isOpen, setIsOpen] = React.useState(false);
-
-  function toggle() {
-    setIsOpen(!isOpen);
-  }
+function FAQ() {
+  const initialData = [
+    {
+      id: 1,
+      question: 'Как проходит обучение?',
+      answer:
+        'После записи на курс вам открывается доступ к учебными материалам. В конце каждой учебной главы необходимо сдать тест, и набрать пороговое значение баллов.',
+    },
+    {
+      id: 2,
+      question: 'Что если я не набрал достаточное количество баллов?',
+      answer:
+        'У вас будет несколько попыток пересдачи теста. Если вы безуспешно используете все попытки, то материалы курса становятся недоступными.',
+    },
+    {
+      id: 3,
+      question: 'Как долго доступны материалы?',
+      answer: 'После окончания материалы доступны бессрочно',
+    },
+  ];
 
   return (
-    <div>
-      <Card className={styles.card}>
-        <div
-          className={styles.container}
-          role="button"
-          tabIndex={0}
-          onKeyDown={toggle}
-          onClick={toggle}
-        >
-          <h2 className={styles.title}>FAQ</h2>
-          <button type="button" className={styles.button}>
-            {isOpen ? 'Свернуть' : 'Развернуть'}
-          </button>
-        </div>
-        <ul
-          className={classNames(styles.list, {
-            [styles.list_show]: isOpen,
-          })}
-        >
-          {data &&
-            data.map((list) => (
-              <li key={list.id} className={styles.list__item}>
-                <Accordion
-                  title={list.question}
-                  button="icon"
-                  className={styles.subtitle}
-                >
-                  <p className={styles.answer}>{list.answer}</p>
-                </Accordion>
-              </li>
-            ))}
+    <Card className={styles.card}>
+      <Accordion className={styles.title} title="FAQ" button="text">
+        <ul className={styles.list}>
+          {initialData.map((list) => (
+            <li key={list.id} className={styles.list__item}>
+              <Accordion
+                title={list.question}
+                button="icon"
+                className={styles.question}
+              >
+                <p className={styles.answer}>{list.answer}</p>
+              </Accordion>
+            </li>
+          ))}
         </ul>
-      </Card>
-    </div>
+      </Accordion>
+    </Card>
   );
 }
-
-FAQ.propTypes = {
-  data: PropTypes.arrayOf(
-    PropTypes.shape({
-      id: PropTypes.number.isRequired,
-      question: PropTypes.string.isRequired,
-      answer: PropTypes.string.isRequired,
-    })
-  ).isRequired,
-};
 
 export default FAQ;
