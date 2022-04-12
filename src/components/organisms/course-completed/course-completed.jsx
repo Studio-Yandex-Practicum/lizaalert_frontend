0b81bf76ba6f6a4579a58766cb665c769f700469
MigelG <za4elovek@yandex.ru>
2022-04-12 17:47:41 +0300

    #124-fix-CourseCompleted-component

--- src/components/organisms/course-completed/course-completed.jsx
+++ src/components/organisms/course-completed/course-completed.jsx
@@ -1,63 +1,75 @@
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 import { Heading } from '../../atoms';
 import { Card } from '../../templates';
 import styles from './course-completed.module.scss';
+import routes from '../../../config/routes';
 
 /**
  * @description Компонент карточки завершения курса.
  *
- * - complete - boolean - успешное завершение курса
+ * - isCompleted - boolean - успешное завершение курса
  * - courseName - string - название курса - обязательный параметр
- * - courseDescription - string - описание успешного прохождения курса
+ * - courseSuccessDescription - string - описание успешного прохождения курса
  */
 
-function CourseCompleted({ complete, courseName, courseDescription }) {
+function CourseCompleted({
+  isCompleted,
+  courseName,
+  courseSuccessDescription,
+}) {
+  const { courses } = routes;
+
   return (
     <Card className={styles.card}>
       <Heading size="l" className={styles.title}>
         Курс завершен
       </Heading>
-      {complete && (
-        <div className={styles.content}>
-          <Heading className={styles.subtitle} level={3} size="xxl">
-            Поздравляем!
-          </Heading>
-          <p
-            className={styles.text}
-          >{`Вы успешно завершили курс «${courseName}».`}</p>
-          <p className={styles.text}>{courseDescription}</p>
-        </div>
-      )}
-      {!complete && (
-        <div className={styles.content}>
-          <Heading className={styles.subtitle} level={3} size="xxl">
-            Сожалеем
-          </Heading>
-          <p className={styles.text}>
-            Курс <span className={styles.text_bold}>{`«${courseName}»`}</span>{' '}
-            не пройден.
-          </p>
-          <p className={styles.text}>
-            Мы предьявляем очень строгие требования к участникам
-            поисково-спасательных мероприятий, из-за чего обучение может быть
-            сложным. Вы&nbsp;можете попробовать себя{' '}
-            <span className={styles.text_bold}>в других направлениях.</span>
-          </p>
-        </div>
-      )}
+
+      <div className={styles.content}>
+        <Heading className={styles.subtitle} level={3} size="xxl">
+          {isCompleted ? 'Поздравляем!' : 'Сожалеем'}
+        </Heading>
+
+        {isCompleted && (
+          <>
+            <p
+              className={styles.text}
+            >{`Вы успешно завершили курс «${courseName}».`}</p>
+            <p className={styles.text}>{courseSuccessDescription}</p>
+          </>
+        )}
+
+        {!isCompleted && (
+          <>
+            <p className={styles.text}>
+              Курс <span className={styles.text_bold}>{`«${courseName}»`}</span>{' '}
+              не пройден.
+            </p>
+            <p className={styles.text}>
+              Мы предьявляем очень строгие требования к участникам
+              поисково-спасательных мероприятий, из-за чего обучение может быть
+              сложным. Вы&nbsp;можете попробовать себя{' '}
+              <Link to={courses.path} className={styles.link}>
+                в других направлениях.
+              </Link>
+            </p>
+          </>
+        )}
+      </div>
     </Card>
   );
 }
 
 CourseCompleted.defaultProps = {
-  complete: false,
-  courseDescription: '',
+  isCompleted: false,
+  courseSuccessDescription: '',
 };
 
 CourseCompleted.propTypes = {
-  complete: PropTypes.bool,
+  isCompleted: PropTypes.bool,
   courseName: PropTypes.string.isRequired,
-  courseDescription: PropTypes.string,
+  courseSuccessDescription: PropTypes.string,
 };
 
 export default CourseCompleted;
