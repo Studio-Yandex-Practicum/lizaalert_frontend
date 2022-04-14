1b32981cbe559b76ffe9dc7f5f3d51972bd2464f
Yuri Zhulev <yuri.zhulyov@gmail.com>
2022-04-14 15:11:54 +0300

    add multiple selectors, add loading render conditions

--- src/components/pages/course/course.jsx
+++ src/components/pages/course/course.jsx
@@ -2,21 +2,33 @@ import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { useParams } from 'react-router-dom';
 import fetchCourseAction from '../../../store/course/thunk';
-import { selectCourse } from '../../../store/course/selectors';
+import {
+  selectCourseLoading,
+  selectCourseTitle,
+} from '../../../store/course/selectors';
 import { CourseBenefits, CourseOverview, CourseContent } from '../../organisms';
 
 function Course() {
   const { courseId } = useParams();
   const dispatch = useDispatch();
-  const course = useSelector(selectCourse);
+  const title = useSelector(selectCourseTitle);
+  const isLoading = useSelector(selectCourseLoading);
 
   useEffect(() => {
     dispatch(fetchCourseAction(courseId));
-  }, []);
+  }, [dispatch]);
+
+  let pageContent;
+
+  if (isLoading) {
+    pageContent = <p>Loading...</p>;
+  } else {
+    pageContent = <h1>{title}</h1>;
+  }
 
   return (
     <div>
-      <p>Single course page. Course id: {course.id}</p>
+      {pageContent}
       <CourseOverview />
       <CourseContent />
       <CourseBenefits />
