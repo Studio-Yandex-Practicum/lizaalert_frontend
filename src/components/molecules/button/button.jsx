4f1c58fdf7e9cb1dba1cf910a033b34bf0fca39c
Tatiana Roslyakova <tat-rs@yandex.ru>
2022-04-27 20:27:51 +0300

    #116 return children: PropTypes.string

--- src/components/molecules/button/button.jsx
+++ src/components/molecules/button/button.jsx
@@ -59,7 +59,7 @@ function Button({
 }
 
 Button.propTypes = {
-  children: PropTypes.oneOfType([PropTypes.string, PropTypes.shape({})]),
+  children: PropTypes.string,
   view: PropTypes.oneOf(['primary', 'secondary', 'text']),
   iconName: PropTypes.string,
   iconPosition: PropTypes.oneOf(['back', 'forward', '']),
