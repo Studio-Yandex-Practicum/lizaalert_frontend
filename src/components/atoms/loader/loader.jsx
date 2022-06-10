f7855f9369d3bb90b998c14527ae275d391f18b3
Alyona Irisova <elaineirr.910@gmail.com>
2022-06-10 13:09:15 +0400

    #151-extend-component-add-overlay-and-position-props

--- src/components/atoms/loader/loader.jsx
+++ src/components/atoms/loader/loader.jsx
@@ -9,27 +9,52 @@ const settings = {
   strokeWidth: 5,
 };
 
-function Loader({ className }) {
+/**
+ * @description Компонент-лоадер, обернутый в div-оверлей с полупрозрачным фоном в цвет основного `background`.
+ * По умолчанию расположен по центру контейнера по обеим осям.
+ *
+ * @props
+ * - className - string - класс-миксин для оверлея
+ * - isFixed - boolean - делает оверлею `position: fixed` и растянутым на ширину и высоту родительского контейнера
+ * - isAbsolute - boolean - делает оверлею `position: absolute` и растянутым на ширину и высоту родительского контейнера
+ * */
+
+function Loader({ className, isFixed, isAbsolute }) {
   return (
-    <svg className={classnames(styles.loader, className)} viewBox="0 0 50 50">
-      <circle
-        className={styles.path}
-        cx={settings.cx}
-        cy={settings.cy}
-        r={settings.radius}
-        fill="none"
-        strokeWidth={settings.strokeWidth}
-      />
-    </svg>
+    <div
+      className={classnames(
+        styles.overlay,
+        {
+          [styles.overlay_pos_fixed]: isFixed,
+          [styles.overlay_pos_absolute]: isAbsolute,
+        },
+        className
+      )}
+    >
+      <svg className={styles.loader} viewBox="0 0 50 50">
+        <circle
+          className={styles.path}
+          cx={settings.cx}
+          cy={settings.cy}
+          r={settings.radius}
+          fill="none"
+          strokeWidth={settings.strokeWidth}
+        />
+      </svg>
+    </div>
   );
 }
 
 Loader.propTypes = {
   className: PropTypes.string,
+  isFixed: PropTypes.bool,
+  isAbsolute: PropTypes.bool,
 };
 
 Loader.defaultProps = {
   className: '',
+  isFixed: false,
+  isAbsolute: false,
 };
 
 export default Loader;
