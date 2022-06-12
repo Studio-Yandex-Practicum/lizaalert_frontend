68f880016955bf65ddca4bd668de02a937525bd0
Tatiana Roslyakova <tat-rs@yandex.ru>
2022-06-12 16:37:18 +0300

    #156 fix fetchAuth thunk

--- src/store/auth/thunk.js
+++ src/store/auth/thunk.js
@@ -4,7 +4,7 @@ import mockAuth from '../../services/mock/auth.json';
 export const fetchAuth = createAsyncThunk(
   'auth/fetchAuth',
   async (authData, { rejectWithValue }) => {
-    const { email, tel } = authData.user;
+    /* const { email, tel } = authData.user; */
     const { isRememberMe } = authData;
     try {
       // eslint-disable-next-line no-inner-declarations
@@ -13,11 +13,10 @@ export const fetchAuth = createAsyncThunk(
         return new Promise((resolve) => setTimeout(resolve(mockAuth), 2000));
       }
       const jwt = await timeout();
-      if (jwt.token) {
-        localStorage.setItem('jwt', jwt.token);
-      }
       if (isRememberMe) {
-        localStorage.setItem('authData', JSON.stringify({ email, tel }));
+        localStorage.setItem('jwt', jwt.token);
+      } else {
+        sessionStorage.setItem('jwt', jwt.token);
       }
       return jwt;
     } catch (error) {
@@ -36,7 +35,7 @@ export const checkAuth = createAsyncThunk(
         return new Promise((resolve) => setTimeout(resolve(), 2000));
       }
       await timeout();
-      return localStorage.getItem('jwt');
+      return localStorage.getItem('jwt') || sessionStorage.getItem('jwt');
     } catch (error) {
       return rejectWithValue(error.message);
     }
