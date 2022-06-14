208106f86d4ca778a2a3a1c4e059491587e65a8b
Alyona Irisova <elaineirr.910@gmail.com>
2022-06-14 18:26:26 +0400

    #151-enchance-tests-description

--- src/components/molecules/loader/loader.test.js
+++ src/components/molecules/loader/loader.test.js
@@ -3,7 +3,7 @@ import Loader from './loader';
 import styles from './loader.module.scss';
 
 describe('Компонент Loader', () => {
-  it('Loader рендерится в DOM', () => {
+  it('Рендерится в DOM', () => {
     const { container } = render(<Loader />);
     const loaderOverlay = container.querySelector(`.${styles.overlay}`);
     expect(loaderOverlay).toBeInTheDocument();
@@ -15,31 +15,31 @@ describe('Компонент Loader', () => {
     expect(loaderPath).toBeInTheDocument();
   });
 
-  it('Loader принимает prop isFixed и при true ставит класс overlay_pos_fixed', () => {
+  it('Принимает prop isFixed и при true ставит класс overlay_pos_fixed', () => {
     const { container } = render(<Loader isFixed />);
     const loaderOverlay = container.querySelector(`.${styles.overlay}`);
     expect(loaderOverlay).toHaveClass(styles.overlay_pos_fixed);
   });
 
-  it('Loader принимает prop isFixed и при false НЕ ставит класс overlay_pos_fixed', () => {
+  it('Принимает prop isFixed и при false НЕ ставит класс overlay_pos_fixed', () => {
     const { container } = render(<Loader isFixed={false} />);
     const loaderOverlay = container.querySelector(`.${styles.overlay}`);
     expect(loaderOverlay).not.toHaveClass(styles.overlay_pos_fixed);
   });
 
-  it('Loader принимает prop isAbsolute и при true ставит класс overlay_pos_absolute', () => {
+  it('Принимает prop isAbsolute и при true ставит класс overlay_pos_absolute', () => {
     const { container } = render(<Loader isAbsolute />);
     const loaderOverlay = container.querySelector(`.${styles.overlay}`);
     expect(loaderOverlay).toHaveClass(styles.overlay_pos_absolute);
   });
 
-  it('Loader принимает prop isAbsolute и при false НЕ ставит класс overlay_pos_absolute', () => {
+  it('Принимает prop isAbsolute и при false НЕ ставит класс overlay_pos_absolute', () => {
     const { container } = render(<Loader isAbsolute={false} />);
     const loaderOverlay = container.querySelector(`.${styles.overlay}`);
     expect(loaderOverlay).not.toHaveClass(styles.overlay_pos_absolute);
   });
 
-  it('Loader принимает prop className', () => {
+  it('Принимает prop className и ставит этот класс на overlay', () => {
     const { container } = render(<Loader className="test" />);
     const loaderOverlay = container.querySelector(`.${styles.overlay}`);
     expect(loaderOverlay).toHaveClass('test');
