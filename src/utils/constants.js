30eac3651fa9922c03e6cbdb0d898b716b4383cb
Tatiana Roslyakova <tat-rs@yandex.ru>
2022-06-17 21:59:40 +0300

    #156 исправить валидацию номера телефона

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -27,10 +27,10 @@ export const RADIO = 'radio';
 
 export const errorMessages = {
   email: 'Введите эл. адрес в формате: anna@liza-alert.ru',
-  tel: 'Введите номер телефона в формате: +7 (XXX)-XXX-XX-XX',
+  tel: 'Введите номер телефона в формате: +7 (XXX) XXX XX XX',
 };
 
 export const patterns = {
   email: '[^\\s@]+@[^\\s@]+\\.[^\\s@]{2,}$',
-  tel: '\\+7\\s?[\\(]{0,1}9[0-9]{2}[\\)]{0,1}\\s?\\d{3}[-]{0,1}\\d{2}[-]{0,1}\\d{2}',
+  tel: '\\+7\\s?[0-9]{10}',
 };
