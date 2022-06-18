7b9c79b77179d0a6ac0b41510c87ca410fce90bd
Alyona Irisova <elaineirr.910@gmail.com>
2022-06-18 12:14:46 +0400

    build/fix-eslint-and-conflicts

--- src/index.tsx
+++ src/index.tsx
@@ -7,17 +7,21 @@ import reportWebVitals from './reportWebVitals';
 import importBuildTarget from './config/import-build-target';
 import './styles/index.scss';
 
-importBuildTarget().then(({ default: Environment }) => {
-  ReactDOM.render(
-    <React.StrictMode>
-      <Provider store={store}>
-        <ErrorBoundary>
-          <Environment />
-        </ErrorBoundary>
-      </Provider>
-    </React.StrictMode>,
-    document.getElementById('root')
-  );
-});
+importBuildTarget()
+  .then(({ default: Environment }) => {
+    ReactDOM.render(
+      <React.StrictMode>
+        <Provider store={store}>
+          <ErrorBoundary>
+            <Environment />
+          </ErrorBoundary>
+        </Provider>
+      </React.StrictMode>,
+      document.getElementById('root')
+    );
+  })
+  .catch(() => {
+    /* TODO: показать окошко, что все пропало */
+  });
 
 reportWebVitals();
