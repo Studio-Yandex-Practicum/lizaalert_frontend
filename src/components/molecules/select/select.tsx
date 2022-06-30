fab0c185061ed31ef819d938a70c0858316a858e
Yuri Zhulev <yuri.zhulyov@gmail.com>
2022-06-30 08:33:52 +0300

    remove linter comments

--- src/components/molecules/select/select.tsx
+++ src/components/molecules/select/select.tsx
@@ -1,6 +1,3 @@
-/* eslint-disable @typescript-eslint/no-unsafe-argument */
-/* eslint-disable @typescript-eslint/no-unsafe-assignment */
-/* eslint-disable @typescript-eslint/no-unsafe-member-access */
 import { useState, KeyboardEvent } from 'react';
 import classnames from 'classnames';
 import { Option } from '../../atoms';
