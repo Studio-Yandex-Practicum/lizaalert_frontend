e5abdfb36e4c50dab0d5012101d66f90eba262ba
Alyona Irisova <elaineirr.910@gmail.com>
2022-07-01 20:35:14 +0400

    #209-update-header-link-to-ts

---
+++ src/components/molecules/header-link/header-link.tsx
@@ -0,0 +1,34 @@
+import { NavLink } from 'react-router-dom';
+import TextWithIcon from '../text-with-icon/text-with-icon';
+import styles from './header-link.module.scss';
+
+export type HeaderLinkProps = {
+  text: string;
+  iconType: string;
+  link: string;
+};
+
+/**
+ * @description Компонент навигационной ссылки для навигации сайта, подсвечивает активный роут
+ *
+ * @props
+ * - text - string - текст ссылки
+ * - iconType - string - тип иконки из объекта `icons`
+ * - link - string - `href` для ссылки
+ * */
+
+function HeaderLink({ text, iconType, link }: HeaderLinkProps) {
+  return (
+    <NavLink className={styles.link} end to={link}>
+      {({ isActive }) => (
+        <TextWithIcon
+          text={text}
+          iconType={iconType}
+          color={isActive ? 'color-hover' : 'dark-primary'}
+        />
+      )}
+    </NavLink>
+  );
+}
+
+export default HeaderLink;
