9345a9efa344fac9a48d391a64f664bd61066258
Alyona Irisova <elaineirr.910@gmail.com>
2022-07-01 18:27:01 +0400

    #205-fix-reexports

--- src/components/atoms/index.ts
+++ src/components/atoms/index.ts
@@ -1,5 +1,5 @@
-export { CardHtmlTags, default as Card } from './card/card';
+export { default as Card, CardHtmlTags } from './card/card';
 export { default as Icon } from './icon/icon';
-export { HeadingSizes, default as Heading } from './heading/heading';
+export { default as Heading, HeadingSizes } from './heading/heading';
 export { default as Option } from './option/option';
 export type { OptionType } from './option/option';
