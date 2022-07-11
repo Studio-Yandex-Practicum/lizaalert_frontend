4d17cdfd0b248259b59b4abed353d5db9a430ed0
Tatiana Roslyakova <tat-rs@yandex.ru>
2022-07-11 12:43:56 +0300

    #235-add-ts-CourseOverview-component

--- src/components/organisms/course-overview/course-overview.tsx
+++ src/components/organisms/course-overview/course-overview.tsx
@@ -1,8 +1,23 @@
-import PropTypes from 'prop-types';
 import { Card } from '../../atoms';
 import { Button, TextWithIcon } from '../../molecules';
 import DefaultImage from '../../../assets/images/course.jpg';
 import styles from './course-overview.module.scss';
+
+type CourseOverviewProps = {
+  imgLink?: string;
+  level?: string;
+  lessonQuantity?: number;
+  duration?: string;
+  startDate?: string;
+};
+
+const defaultProps = {
+  imgLink: DefaultImage,
+  level: 'Бывалый',
+  lessonQuantity: 24,
+  duration: '64 ч',
+  startDate: '22.02.2022',
+};
 
 function CourseOverview({
   imgLink,
@@ -10,7 +25,7 @@ function CourseOverview({
   lessonQuantity,
   startDate,
   duration,
-}) {
+}: CourseOverviewProps) {
   return (
     <Card className={styles.courseOverview} noPadding>
       <div className={styles.courseImageContainer}>
@@ -50,20 +65,6 @@ function CourseOverview({
   );
 }
 
-CourseOverview.defaultProps = {
-  imgLink: DefaultImage,
-  level: 'Бывалый',
-  lessonQuantity: 24,
-  duration: '64 ч',
-  startDate: '22.02.2022',
-};
-
-CourseOverview.propTypes = {
-  imgLink: PropTypes.string,
-  level: PropTypes.string,
-  lessonQuantity: PropTypes.number,
-  duration: PropTypes.string,
-  startDate: PropTypes.string,
-};
+CourseOverview.defaultProps = defaultProps;
 
 export default CourseOverview;
