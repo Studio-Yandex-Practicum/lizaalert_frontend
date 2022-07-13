2a93c3ab636f0c6b4448ef58596fdcda69190d37
Yura <33540575+Yurikze@users.noreply.github.com>
2022-07-13 17:50:09 +0300

    237 add typescript (#263)

--- src/components/organisms/course-preview/course-preview.tsx
+++ src/components/organisms/course-preview/course-preview.tsx
@@ -1,11 +1,45 @@
-import PropTypes from 'prop-types';
 import { useNavigate } from 'react-router-dom';
 import { Card, Heading } from '../../atoms';
 import { Button, Tag, TextWithIcon } from '../../molecules';
 import getDeclensionOf from '../../../utils/getDeclensionOf';
 import styles from './course-preview.module.scss';
 
-function CoursePreview({ course }) {
+export type LevelType = {
+  id: number;
+  name: string;
+  slug: string;
+};
+
+export type StatusType = {
+  id: number;
+  name: string;
+  buttonName: string;
+  slug: string;
+};
+
+export type CourseType = {
+  id: number;
+  level: LevelType;
+  title: string;
+  description: string;
+  image: string;
+  duration: number;
+  lessonsCount: number;
+  status: StatusType;
+};
+
+type CoursePreviewProps = {
+  course: CourseType;
+};
+
+/**
+ * @description Компонент предпросмотра курса
+ *
+ * @props
+ * course - obj - объект Объект курса содержит id, level, title, description, image, duration, lessonsCount, status
+ */
+
+function CoursePreview({ course }: CoursePreviewProps) {
   const navigate = useNavigate();
   const {
     id,
@@ -45,27 +79,5 @@ function CoursePreview({ course }) {
     </Card>
   );
 }
-
-CoursePreview.propTypes = {
-  course: PropTypes.shape({
-    id: PropTypes.number.isRequired,
-    level: PropTypes.shape({
-      id: PropTypes.number.isRequired,
-      name: PropTypes.string.isRequired,
-      slug: PropTypes.string.isRequired,
-    }).isRequired,
-    title: PropTypes.string.isRequired,
-    description: PropTypes.string.isRequired,
-    image: PropTypes.string.isRequired,
-    duration: PropTypes.number.isRequired,
-    lessonsCount: PropTypes.number.isRequired,
-    status: PropTypes.shape({
-      id: PropTypes.number.isRequired,
-      name: PropTypes.string.isRequired,
-      buttonName: PropTypes.string.isRequired,
-      slug: PropTypes.string.isRequired,
-    }).isRequired,
-  }).isRequired,
-};
 
 export default CoursePreview;
