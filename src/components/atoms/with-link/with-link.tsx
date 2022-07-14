7ad4b1f4e1fb87ecf5263ed0b8c779adc7320373
Alyona Irisova <elaineirr.910@gmail.com>
2022-07-14 16:58:34 +0400

    #264-add-with-link-hoc

---
+++ src/components/atoms/with-link/with-link.tsx
@@ -0,0 +1,52 @@
+import { FunctionComponent } from 'react';
+import { Link } from 'react-router-dom';
+import classnames from 'classnames';
+import styles from './with-link.module.scss';
+
+type WithLinkProps = {
+  component: FunctionComponent;
+  href: string;
+  isExternal?: boolean;
+  weight?: 'bold' | 'semibold' | 'normal';
+  className?: string;
+};
+
+const defaultProps = {
+  isExternal: false,
+  className: '',
+  weight: 'normal',
+};
+
+function WithLink({
+  component: Component,
+  isExternal,
+  href,
+  weight = 'normal',
+  className,
+  ...props
+}: WithLinkProps) {
+  const classNames = classnames(styles.link, styles[weight], className);
+
+  if (isExternal) {
+    return (
+      <a
+        className={classNames}
+        href={href}
+        rel="noopener noreferrer"
+        target="_blank"
+      >
+        <Component {...props} />
+      </a>
+    );
+  }
+
+  return (
+    <Link className={classNames} to={href}>
+      <Component {...props} />
+    </Link>
+  );
+}
+
+WithLink.defaultProps = defaultProps;
+
+export default WithLink;
--- src/components/pages/development/development.jsx
+++ src/components/pages/development/development.jsx
@@ -1,5 +1,4 @@
-import { Link } from 'react-router-dom';
-import { Heading } from '../../atoms';
+import { Heading, WithLink } from '../../atoms';
 import { TextWithIcon } from '../../molecules';
 import styles from './development.module.scss';
 import { routes } from '../../../config';
@@ -8,9 +7,12 @@ function Development() {
   return (
     <main className={styles.development}>
       <Heading level={1} title="Страница находится в разработке" />
-      <Link to={routes.courses.path} className={styles.link}>
-        <TextWithIcon iconType="course" text="Вернуться к курсам" />
-      </Link>
+      <WithLink
+        component={TextWithIcon}
+        href={routes.courses.path}
+        iconType="course"
+        text="Вернуться к курсам"
+      />
     </main>
   );
 }
