3cdc8c958e998d987e778f4cb5e04253ed4a9af2
Alyona Irisova <elaineirr.910@gmail.com>
2022-08-26 20:40:39 +0400

    #195-update-store-to-ts

---
+++ src/store/index.ts
@@ -0,0 +1,44 @@
+import {
+  Action,
+  CombinedState,
+  combineReducers,
+  configureStore,
+} from '@reduxjs/toolkit';
+import { TypedUseSelectorHook, useSelector } from 'react-redux';
+import { isDevEnv } from 'config';
+import coursesReducer from './courses/slice';
+import testReducer from './test/slice';
+import courseReducer from './course/slice';
+import lessonReducer from './lesson/slice';
+import profileReducer from './profile/slice';
+import authReducer from './auth/slice';
+import { AppState } from './types';
+
+const appReducer = combineReducers<AppState>({
+  courses: coursesReducer,
+  test: testReducer,
+  course: courseReducer,
+  lesson: lessonReducer,
+  profile: profileReducer,
+  auth: authReducer,
+});
+
+const rootReducer = (
+  state: CombinedState<AppState> | undefined,
+  action: Action
+) => {
+  if (action.type === 'courses/resetAllState') {
+    // Возврат стора к дефолтному состоянию
+    return appReducer(undefined, action);
+  }
+
+  return appReducer(state, action);
+};
+
+export const store = configureStore({
+  reducer: rootReducer,
+  devTools: isDevEnv,
+});
+
+/** Типизированный хук селектора, использовать его в компонентах */
+export const useAppSelector: TypedUseSelectorHook<AppState> = useSelector;
---
+++ src/store/types.ts
@@ -0,0 +1,9 @@
+// После типизации конкретного стора заменить any на тип стора
+export type AppState = {
+  auth: any;
+  courses: any;
+  course: any;
+  lesson: any;
+  test: any;
+  profile: any;
+};
