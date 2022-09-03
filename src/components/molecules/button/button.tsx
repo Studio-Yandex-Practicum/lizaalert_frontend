57f29143fc05aebef884f491f2c62832a8e7abda
AnnSyh <ann.syharevskaya@gmail.com>
2022-09-04 00:35:18 +0300

    #279-btn-orange-border-fix-after-review

--- src/components/molecules/button/button.tsx
+++ src/components/molecules/button/button.tsx
@@ -11,7 +11,7 @@ import { ButtonProps } from './types';
  * - children - string - текст кнопки, имеет приоритет перед `text`
  * - text - string - текст кнопки, альтернатива для `children`
  * - view - enum ('primary' | 'secondary' | 'tertiary' | 'text') - внешний вид кнопки
- * - hover - enum ('default' | 'border') - внешний вид обводки кнопки
+ * - hover - enum ('hover-default' | 'hover-border') - внешний вид обводки кнопки
  * - iconName - string - имя иконки из объекта icons
  * - iconPosition - enum ('back' | 'forward') - позиционирование иконки слева/справа от текста
  * - className - string - класс-миксин
@@ -24,7 +24,7 @@ function Button({
   children = null,
   text = '',
   view = 'primary',
-  hover = 'default',
+  hover = 'hover-default',
   iconName = null,
   iconPosition = null,
   className = '',
@@ -36,18 +36,14 @@ function Button({
   const btnClasses = classnames(
     styles.button,
     styles[view],
-    styles[hover],
+    { [styles[`${hover}`]]: view === 'secondary' },
     className
   );
 
   return (
     <button
       {...props}
-      className={classnames(
-        styles.button,
-        { [styles.hover]: styles[view] === 'secondary' },
-        btnClasses
-      )}
+      className={classnames(styles.button, btnClasses)}
       type={type === 'submit' ? 'submit' : 'button'}
     >
       {iconPosition === 'back' && iconName && (
