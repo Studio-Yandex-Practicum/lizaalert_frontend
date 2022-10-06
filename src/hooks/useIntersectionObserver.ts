0d27f2a0d9aea3342dd79eccd29abb56e8f5ca74
Alyona Irisova <elaineirr.910@gmail.com>
2022-10-06 17:45:42 +0400

    #146-added-useIntersectionObserver-hook

---
+++ src/hooks/useIntersectionObserver.ts
@@ -0,0 +1,51 @@
+import React, { useLayoutEffect, useRef } from 'react';
+
+type IntersectionObserverConfig = {
+  elementRef: React.MutableRefObject<null>;
+  callbackOnIntersect: () => void;
+  callbackOnHide?: () => void;
+  rootMargin?: string;
+  threshold?: number;
+};
+
+/*
+ * Хук используется для подгрузки данных и других манипуляций при появлении elementRef во вьюпорте
+ * */
+
+const useIntersectionObserver = ({
+  elementRef,
+  callbackOnIntersect,
+  callbackOnHide,
+  rootMargin,
+  threshold,
+}: IntersectionObserverConfig) => {
+  const observer = useRef<null | IntersectionObserver>(null);
+
+  useLayoutEffect(() => {
+    if (elementRef?.current) {
+      const options = {
+        root: null,
+        rootMargin: rootMargin ?? '0px',
+        threshold: threshold ?? 0,
+      };
+
+      observer.current = new IntersectionObserver(([target]) => {
+        if (target.isIntersecting) {
+          callbackOnIntersect();
+        } else if (callbackOnHide) {
+          callbackOnHide();
+        }
+      }, options);
+
+      observer.current.observe(elementRef.current);
+    }
+
+    return () => {
+      if (observer?.current && elementRef?.current) {
+        observer.current.unobserve(elementRef.current);
+      }
+    };
+  }, [callbackOnIntersect, callbackOnHide, elementRef]);
+};
+
+export default useIntersectionObserver;
