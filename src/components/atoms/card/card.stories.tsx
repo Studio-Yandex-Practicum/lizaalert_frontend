a4e73671ddfcb59b632766018fd69042fa8dddd1
Alyona Irisova <elaineirr.910@gmail.com>
2022-10-09 20:31:46 +0400

    #330-added-card-stories

---
+++ src/components/atoms/card/card.stories.tsx
@@ -0,0 +1,31 @@
+import { ComponentMeta, ComponentStory } from '@storybook/react';
+import Card from './card';
+import { CardProps } from './types';
+
+type TemplateProps = CardProps & {
+  content?: string;
+};
+
+export default {
+  title: 'Atoms/Card',
+  component: Card,
+  argTypes: {
+    content: { type: 'string', defaultValue: 'Контент карточки' },
+  },
+} as ComponentMeta<typeof Card>;
+
+const Template: ComponentStory<typeof Card> = ({
+  content,
+  ...args
+}: TemplateProps) => (
+  <Card {...args}>
+    <span>{content}</span>
+  </Card>
+);
+
+export const WithoutPadding = Template.bind({});
+WithoutPadding.args = {
+  noPadding: true,
+};
+
+export const Standard = Template.bind({});
--- src/components/atoms/card/types.ts
+++ src/components/atoms/card/types.ts
@@ -3,8 +3,20 @@ import { ReactNode } from 'react';
 export type CardHtmlTags = 'article' | 'div' | 'li' | 'aside' | 'section';
 
 export type CardProps = {
+  /**
+   * css-миксин для стилизации
+   * */
   className?: string;
+  /**
+   * Проп, обнуляющий паддинги. По умолчанию `false`, т.е. у карточки имеются стандартные паддинги 32px
+   * */
   noPadding?: boolean;
+  /**
+   * Тип тега-контейнера для семантики: 'article', 'div', 'li', 'aside'. По умолчанию `div`.
+   * */
   htmlTag?: CardHtmlTags;
+  /**
+   * Контент, который нужно вставить в карточку
+   * */
   children: ReactNode;
 };
