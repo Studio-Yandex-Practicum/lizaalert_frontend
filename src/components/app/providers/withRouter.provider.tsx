b49fd5edd55b98cf7f5a21de34bc183d20fa368d
Alyona Irisova <elaineirr.910@gmail.com>
2022-10-10 22:01:05 +0400

    refactor-app-providers

--- src/components/app/providers/withRouter.provider.tsx
+++ src/components/app/providers/withRouter.provider.tsx
@@ -3,7 +3,6 @@ import { BrowserRouter } from 'react-router-dom';
 import { Loader } from 'components/molecules/loader';
 import { ProviderComponent } from './types';
 
-/* eslint react/function-component-definition: 0 */
 export const withRouter = (Component: ProviderComponent) => () =>
   (
     <BrowserRouter>
