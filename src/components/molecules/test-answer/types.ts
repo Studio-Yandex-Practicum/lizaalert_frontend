26c296c623893016a945548c821532304ed19af6
Alyona Irisova <elaineirr.910@gmail.com>
2022-10-19 13:41:36 +0400

    #330-update-test-answer-component

--- src/components/molecules/test-answer/types.ts
+++ src/components/molecules/test-answer/types.ts
@@ -1,14 +1,35 @@
 export type TestAnswerType = {
+  /**
+   * id ответа.
+   * */
   id: number;
+  /**
+   * Текст ответа.
+   * */
   text: string;
+  /**
+   * Флаг правильности ответа.
+   * */
   isCorrect: boolean;
+  /**
+   * Флаг, отмечен ли ответ пользователем.
+   * */
   isChecked: boolean;
 };
 
 export type TestAnswerOptionsType = 'checkbox' | 'radio';
 
 export type TestAnswerProps = {
+  /**
+   * Объект ответа, содержит id (number), text (string), isChecked (boolean), isCorrect (boolean).
+   * */
   answer: TestAnswerType;
+  /**
+   * id вопроса, в котором содержится answer.
+   * */
   questionId: number;
+  /**
+   * Тип чекбокса: мультивыбор или радио.
+   * */
   answerOptions: TestAnswerOptionsType;
 };
