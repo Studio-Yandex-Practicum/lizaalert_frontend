663251dcd9e9d94762c26135ea6c11486f5aad24
Alyona Irisova <elaineirr.910@gmail.com>
2022-10-20 17:19:47 +0400

    #330-added-preview-webinar-stories

--- src/components/organisms/preview-webinar/types.ts
+++ src/components/organisms/preview-webinar/types.ts
@@ -1,4 +1,14 @@
 export type PreviewWebinarProps = {
+  /**
+   * Дата вебинара в формате YYYY-MM-DDTHH:mm:ss.
+   * */
   date: string;
+  /**
+   * Внешняя ссылка на вебинар.
+   * */
   link: string;
+  /**
+   * Миксин для стилизации. Используйте css-класс, чтобы изменить css-свойства элемента.
+   * */
+  className?: string;
 };
