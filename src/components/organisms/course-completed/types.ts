31de5c4dec0a4d98257a4e75a09c402e277674f2
Alyona Irisova <elaineirr.910@gmail.com>
2022-10-20 16:18:21 +0400

    #330-added-course-completed-stories

--- src/components/organisms/course-completed/types.ts
+++ src/components/organisms/course-completed/types.ts
@@ -1,6 +1,22 @@
 export type CourseCompletedProps = {
+  /**
+   * Флаг успешного завершения курса.
+   * */
   isCompleted?: boolean;
+  /**
+   * Полное название курса.
+   * */
   courseName: string;
+  /**
+   * Описание успешного прохождения курса.
+   * */
   courseSuccessDescription?: string;
+  /**
+   * href ссылки для неудачного прохождения.
+   * */
   linkHref: string;
+  /**
+   * Миксин для стилизации. Используйте css-класс, чтобы изменить css-свойства элемента.
+   * */
+  className?: string;
 };
