795bd0a3be3e4f7fa06e2e24d47baaa26d23f27c
Iriswewq <75016726+elaineir@users.noreply.github.com>
2023-02-09 20:52:19 +0400

    enhancement/add-useEvent-hook (#343)
    * enhancement/add-useEvent-hook
    
    * fix-any

---
+++ src/hooks/use-event.ts
@@ -0,0 +1,28 @@
+import { useLayoutEffect, useRef } from 'react';
+
+/**
+ * Почти как useCallback, но с важными отличиями:
+ * - Возвращаемая функция является стабильной ссылкой
+ * - Нет спсика зависимостей
+ * - Имеет доступ к текущим значениям переменных снаружи
+ */
+
+export function useEvent<T extends AnyFunction>(callback: T): T {
+  const latestRef = useRef<T>(callback);
+
+  useLayoutEffect(() => {
+    latestRef.current = callback;
+  }, [callback]);
+
+  const stableRef = useRef<T | null>(null);
+
+  if (!stableRef.current) {
+    stableRef.current = function handler(this: never) {
+      // Здесь не нужны rest-параметры
+      // eslint-disable-next-line prefer-rest-params
+      return latestRef.current.apply(this, arguments as never);
+    } as T;
+  }
+
+  return stableRef.current;
+}
--- src/hooks/use-form-with-validation.ts
+++ src/hooks/use-form-with-validation.ts
@@ -1,4 +1,13 @@
-import { ChangeEvent, useCallback, useState } from 'react';
+import { ChangeEvent, useState } from 'react';
+import { useEvent } from './use-event';
+
+type Values<T> = Partial<T>;
+type Errors<T> = Partial<Record<keyof T, string>>;
+type ResetFunction<T> = (
+  values: Values<T>,
+  errors: Errors<T>,
+  isValid: boolean
+) => void;
 
 /**
  * Хук реализует логику хранения и обработки данных формы.
@@ -9,11 +18,11 @@ import { ChangeEvent, useCallback, useState } from 'react';
  * */
 
 const useFormWithValidation = <T extends Record<string, unknown>>() => {
-  const [values, setValues] = useState<Partial<T>>({});
-  const [errors, setErrors] = useState<Partial<Record<keyof T, string>>>({});
+  const [values, setValues] = useState<Values<T>>({});
+  const [errors, setErrors] = useState<Errors<T>>({});
   const [isValid, setIsValid] = useState(false);
 
-  const handleChange = (evt: ChangeEvent<HTMLInputElement>) => {
+  const handleChange = useEvent((evt: ChangeEvent<HTMLInputElement>) => {
     const { target } = evt;
     const { name, value } = target;
     setValues({ ...values, [name]: value });
@@ -22,41 +31,39 @@ const useFormWithValidation = <T extends Record<string, unknown>>() => {
     if (form) {
       setIsValid(form.checkValidity());
     }
-  };
+  });
 
-  const handleChangeFiles = (
-    evt: ChangeEvent<HTMLInputElement>,
-    pattern: RegExp
-  ) => {
-    if (!evt.target.files) {
-      return;
+  const handleChangeFiles = useEvent(
+    (evt: ChangeEvent<HTMLInputElement>, pattern: RegExp) => {
+      if (!evt.target.files) {
+        return;
+      }
+      const file = evt.target.files[0];
+      const { name, value } = evt.target;
+      if (!file) {
+        return;
+      }
+      if (file && pattern.test(file.name)) {
+        setValues({ ...values, [name]: value });
+        setErrors({ ...errors, [name]: '' });
+      } else {
+        setErrors({ ...errors, [name]: 'Некорректный формат файла' });
+        setIsValid(false);
+        return;
+      }
+      const form = evt.target.closest('form');
+      if (form) {
+        setIsValid(form.checkValidity());
+      }
     }
-    const file = evt.target.files[0];
-    const { name, value } = evt.target;
-    if (!file) {
-      return;
-    }
-    if (file && pattern.test(file.name)) {
-      setValues({ ...values, [name]: value });
-      setErrors({ ...errors, [name]: '' });
-    } else {
-      setErrors({ ...errors, [name]: 'Некорректный формат файла' });
-      setIsValid(false);
-      return;
-    }
-    const form = evt.target.closest('form');
-    if (form) {
-      setIsValid(form.checkValidity());
-    }
-  };
+  );
 
-  const resetForm = useCallback(
+  const resetForm = useEvent<ResetFunction<T>>(
     (newValues = {}, newErrors = {}, newIsValid = false) => {
       setValues(newValues);
       setErrors(newErrors);
       setIsValid(newIsValid);
-    },
-    [setValues, setErrors, setIsValid]
+    }
   );
 
   return {
