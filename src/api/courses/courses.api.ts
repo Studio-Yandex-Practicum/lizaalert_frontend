c65c82722bfcc7919461a1d5f6a9bb7bc49f9b1b
Alena I <elaineirr.910@gmail.com>
2023-07-31 17:04:01 +0400

    #349-fix-api-request-in-mock-mode

--- src/api/courses/courses.api.ts
+++ src/api/courses/courses.api.ts
@@ -6,12 +6,13 @@ const SERVICE_URL = '/courses/';
 class CoursesApi extends BaseApi {
   getCourses = ({ page, pageSize }: GetCoursesDataModel) =>
     this.createRequest<CoursesModel>({
-      request: this.api.get(SERVICE_URL, {
-        params: {
-          page,
-          page_size: pageSize,
-        },
-      }),
+      request: () =>
+        this.api.get(SERVICE_URL, {
+          params: {
+            page,
+            page_size: pageSize,
+          },
+        }),
       mock: () => import('./mock/courses'),
     });
 }
