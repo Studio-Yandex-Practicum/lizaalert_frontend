71f3de5d25fee5799b24cd9e85b0600db798f928
Maria Gromyko <102763756+MariaGrom@users.noreply.github.com>
2023-08-14 19:03:28 +0300

    #370-use-formik-and-yup-for-login-and-register-forms (#371)
    * add-formik-and-yup
    
    * fix: update branch to branch master
    
    * add formik and yup to the login-form
    
    * fix: deleting redundant comments
    
    * fix: delete dependencies lefthook-darwin-x64

--- src/components/organisms/register-form/register-form.tsx
+++ src/components/organisms/register-form/register-form.tsx
@@ -1,28 +1,51 @@
-import type { FC, FormEvent } from 'react';
+import type { FC } from 'react';
+import { FormikHelpers, useFormik } from 'formik';
 import { Card } from 'components/atoms/card';
 import { Heading } from 'components/atoms/typography';
 import { Button } from 'components/molecules/button';
 import { Input } from 'components/molecules/input';
 import { StyledLink } from 'components/molecules/styled-link';
-import { useFormWithValidation } from 'hooks/use-form-with-validation';
 import { routes } from 'config';
-import { ErrorMessages, Patterns } from 'utils/constants';
+import { getValidationSchema } from 'utils/validation';
 import type { UserRegisterFormData } from './types';
 import styles from './register-form.module.scss';
+
+const schema = getValidationSchema<UserRegisterFormData>(
+  'email',
+  'phone',
+  'password',
+  'confirmPassword'
+);
+
+const initialValues: UserRegisterFormData = {
+  email: '',
+  phone: '',
+  password: '',
+  confirmPassword: '',
+};
 
 /**
  * Компонент-форма регистрации пользователя.
  * */
 
 export const RegisterForm: FC = () => {
-  const { values, handleChange, errors, isValid } =
-    useFormWithValidation<UserRegisterFormData>();
-
-  const handleSubmit = (evt: FormEvent<HTMLFormElement>) => {
-    evt.preventDefault();
-
+  const handleSubmit = async (
+    values: UserRegisterFormData,
+    { validateForm }: FormikHelpers<UserRegisterFormData>
+  ) => {
+    await validateForm(values);
+    console.log(values);
     // TODO запрос на регистрацию
   };
+
+  const formik = useFormik<UserRegisterFormData>({
+    initialValues,
+    validationSchema: schema,
+    onSubmit: handleSubmit,
+  });
+
+  const areAllValuesSet =
+    Object.keys(formik.values).length === Object.keys(formik.touched).length;
 
   return (
     <Card className={styles.container} htmlTag="section">
@@ -34,57 +57,58 @@ export const RegisterForm: FC = () => {
         className={styles.heading}
       />
 
-      <form onSubmit={handleSubmit} name="registerForm" className={styles.form}>
+      <form
+        onSubmit={formik.handleSubmit}
+        name="registerForm"
+        className={styles.form}
+        noValidate
+      >
         <Input
           labelName="Email"
           name="email"
           type="email"
-          value={values?.email || ''}
+          value={formik.values.email}
           placeholder="Введите адрес электронной почты"
-          pattern={Patterns.email}
-          isValid={!errors.email}
-          error={ErrorMessages.email}
-          onChange={handleChange}
-          required
+          isValid={!formik.touched.email || !formik.errors.email}
+          error={formik.errors.email}
+          onChange={formik.handleChange}
         />
         <Input
           labelName="Номер телефона"
-          name="tel"
+          name="phone"
           type="tel"
-          value={values?.tel || ''}
+          value={formik.values.phone}
           placeholder="+7 ( ___ ) ___  -  ___"
-          pattern={Patterns.tel}
-          isValid={!errors.tel}
-          error={ErrorMessages.tel}
-          onChange={handleChange}
-          required
+          isValid={!formik.touched.phone || !formik.errors.phone}
+          error={formik.errors.phone}
+          onChange={formik.handleChange}
         />
         <Input
           labelName="Пароль"
           name="password"
           type="password"
-          value={values?.password || ''}
+          value={formik.values.password}
           placeholder=""
-          isValid={!errors.password}
-          error={errors.password}
-          onChange={handleChange}
-          required
+          isValid={!formik.touched.password || !formik.errors.password}
+          error={formik.errors.password}
+          onChange={formik.handleChange}
         />
         <Input
           labelName="Подтверждение пароля"
           name="confirmPassword"
           type="password"
-          value={values?.confirmPassword || ''}
+          value={formik.values.confirmPassword}
           placeholder=""
-          isValid={values.password === values.confirmPassword}
-          error={ErrorMessages.confirmPassword}
-          onChange={handleChange}
-          required
+          isValid={
+            !formik.touched.confirmPassword || !formik.errors.confirmPassword
+          }
+          error={formik.errors.confirmPassword}
+          onChange={formik.handleChange}
         />
         <Button
           className={styles.button}
           type="submit"
-          disabled={!isValid}
+          disabled={areAllValuesSet && (formik.isSubmitting || !formik.isValid)}
           text="Регистрация"
         />
       </form>
