6804ac9fb21056cebdec6da73d26caac26287010
Karolina Lipnitskaite <lipnitskaite@gmail.com>
2023-08-31 16:31:43 +0200

    #359-remove-course-tasks-hard-code

--- src/components/organisms/course-description/course-description.tsx
+++ src/components/organisms/course-description/course-description.tsx
@@ -1,7 +1,7 @@
 import type { FC } from 'react';
 import classnames from 'classnames';
 import { Card } from 'components/atoms/card';
-import { Heading, Li, P } from 'components/atoms/typography';
+import { P } from 'components/atoms/typography';
 import { Accordion } from 'components/molecules/accordion';
 import styles from './course-description.module.scss';
 import { defaultProps } from './constants';
@@ -13,7 +13,6 @@ import type { CourseDescriptionProps } from './types';
 
 export const CourseDescription: FC<CourseDescriptionProps> = ({
   description = defaultProps.description,
-  tasks = defaultProps.tasks,
   className,
 }) => (
   <Card className={classnames(styles.description, className)} htmlTag="section">
@@ -24,24 +23,6 @@ export const CourseDescription: FC<CourseDescriptionProps> = ({
       open
     >
       <P className={styles.text} text={description} />
-
-      <Heading
-        level={3}
-        text="Основные задачи нашего подразделения:"
-        size="m"
-        weight="bold"
-        className={styles.titleTasks}
-      />
-
-      {tasks?.length > 0 && (
-        <ul className={styles.tasksList}>
-          {tasks.map((task) => (
-            <Li key={task} className={styles.task}>
-              {task}
-            </Li>
-          ))}
-        </ul>
-      )}
     </Accordion>
   </Card>
 );
--- src/components/organisms/course-description/types.ts
+++ src/components/organisms/course-description/types.ts
@@ -1,8 +1,6 @@
 export type CourseDescriptionProps = {
   /** Описание курса. */
   description?: string;
-  /** Список задач курса в виде массива. */
-  tasks?: string[];
   /** Миксин для стилизации. Используйте css-класс, чтобы изменить css-свойства элемента. */
   className?: string;
 };
