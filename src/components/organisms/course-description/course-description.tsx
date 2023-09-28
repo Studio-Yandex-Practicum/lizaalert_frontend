2903daed57bad932d9d90c798c89dd5a19199619
Karolina Lipnitskaite <lipnitskaite@gmail.com>
2023-09-28 16:31:37 +0200

    #359-delete-tasks-component

--- src/components/organisms/course-description/course-description.tsx
+++ src/components/organisms/course-description/course-description.tsx
@@ -2,7 +2,6 @@ import type { FC } from 'react';
 import classnames from 'classnames';
 import ReactMarkdown from 'react-markdown';
 import { Card } from 'components/atoms/card';
-import { Heading } from 'components/atoms/typography';
 import { Accordion } from 'components/molecules/accordion';
 import styles from './course-description.module.scss';
 import { defaultProps } from './constants';
@@ -14,7 +13,6 @@ import type { CourseDescriptionProps } from './types';
 
 export const CourseDescription: FC<CourseDescriptionProps> = ({
   description = defaultProps.description,
-  tasks = defaultProps.tasks,
   className,
 }) => (
   <Card className={classnames(styles.description, className)} htmlTag="section">
@@ -25,19 +23,6 @@ export const CourseDescription: FC<CourseDescriptionProps> = ({
       open
     >
       <ReactMarkdown className={styles.text}>{description}</ReactMarkdown>
-
-      {!!tasks && (
-        <>
-          <Heading
-            level={3}
-            text="Основные задачи нашего подразделения:"
-            size="m"
-            weight="bold"
-            className={styles.titleTasks}
-          />
-          <ReactMarkdown className={styles.tasksList}>{tasks}</ReactMarkdown>
-        </>
-      )}
     </Accordion>
   </Card>
 );
