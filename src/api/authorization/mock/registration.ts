6337cf9085d4a68b176530ed33201e32f913c5d3
Alena I <elaineirr.910@gmail.com>
2023-10-30 11:54:47 +0400

    #418-updates

--- src/api/authorization/mock/registration.ts
+++ src/api/authorization/mock/registration.ts
@@ -1,15 +1,13 @@
 import type { RegistrationModel } from '../types';
 
-const registrationData = new Promise<RegistrationModel>((resolve) => {
-  resolve({
-    user: {
-      username: 'userexample',
-      email: 'example@mail.ru',
-      id: 2,
-    },
-    access_token: 'exampleaccesstoken',
-    refresh_token: 'examplerefreshtoken',
-  });
-});
+const registrationData: RegistrationModel = {
+  user: {
+    username: 'userexample',
+    email: 'example@mail.ru',
+    id: 2,
+  },
+  access_token: 'exampleaccesstoken',
+  refresh_token: 'examplerefreshtoken',
+};
 
-export default registrationData;
+export default Promise.resolve(() => registrationData);
