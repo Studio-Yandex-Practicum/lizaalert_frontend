8b3ab70b512d6b4baf3f7874fc9bd02b5a60a683
Alena I <elaineirr.910@gmail.com>
2023-12-19 15:34:49 +0400

    #444-update

--- src/components/organisms/restore-password-form/restore-password-form.tsx
+++ src/components/organisms/restore-password-form/restore-password-form.tsx
@@ -6,7 +6,7 @@ import { Heading } from 'components/atoms/typography';
 import { Button } from 'components/molecules/button';
 import { Input } from 'components/molecules/input';
 import { useFormWithValidation } from 'hooks/use-form-with-validation';
-import { RegExpPatterns } from 'utils/constants';
+import { RegExpPatterns, UserDataFieldNames } from 'utils/constants';
 import styles from './restore-password-form.module.scss';
 import type { RestorePasswordFormData } from './types';
 
@@ -32,11 +32,11 @@ export const RestorePasswordForm: FC = () => {
       <form className={styles.form} onSubmit={handleFormSubmit}>
         <Input
           labelName="Email"
-          name="email"
-          value={values.email || ''}
+          name={UserDataFieldNames.Email}
+          value={values[UserDataFieldNames.Email] || ''}
           onChange={handleChange}
-          error={errors.email}
-          isValid={!errors.email}
+          error={errors[UserDataFieldNames.Email]}
+          isValid={!errors[UserDataFieldNames.Email]}
           pattern={RegExpPatterns.email}
           type="email"
           placeholder="Ведите адрес электронной почты"
