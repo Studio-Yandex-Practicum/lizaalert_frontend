37cde7b04d0db800f4544d30f7f3c3a8df90b40c
shinonhorror <shinonhorror@gmail.com>
2024-02-05 16:17:21 +0300

    TM-32-return-function

--- src/store/profile/thunk.ts
+++ src/store/profile/thunk.ts
@@ -1,29 +1,31 @@
 import { createAsyncThunk } from '@reduxjs/toolkit';
-import type { AccountFormData } from 'api/profile';
+import type { AccountFormData, ProfileModel } from 'api/profile';
 import { profileApi } from 'api/profile/profile.api';
+
+const transformProfileData = (profile: ProfileModel) => ({
+  id: profile.id,
+  accountData: {
+    phone_number: profile.phone_number,
+    email: profile.email,
+  },
+  personalData: {
+    full_name: profile.full_name,
+    birth_date: profile.birth_date,
+    location: profile.location,
+  },
+  accountOverview: {
+    full_name: profile.full_name,
+    count_pass_course: profile.count_pass_course,
+    department: profile.department,
+    call_sign: profile.call_sign,
+    photo: profile.photo,
+  },
+});
 
 export const fetchProfile = createAsyncThunk('profile/fetch', async () => {
   const profile = await profileApi.getProfile();
 
-  return {
-    id: profile.id,
-    accountData: {
-      phone_number: profile.phone_number,
-      email: profile.email,
-    },
-    personalData: {
-      full_name: profile.full_name,
-      birth_date: profile.birth_date,
-      location: profile.location,
-    },
-    accountOverview: {
-      full_name: profile.full_name,
-      count_pass_course: profile.count_pass_course,
-      department: profile.department,
-      call_sign: profile.call_sign,
-      photo: profile.photo,
-    },
-  };
+  return transformProfileData(profile);
 });
 
 export const editProfile = createAsyncThunk(
@@ -31,24 +33,6 @@ export const editProfile = createAsyncThunk(
   async (profileData: AccountFormData | FormData) => {
     const profile = await profileApi.editProfile(profileData);
 
-    return {
-      id: profile.id,
-      accountData: {
-        phone_number: profile.phone_number,
-        email: profile.email,
-      },
-      personalData: {
-        full_name: profile.full_name,
-        birth_date: profile.birth_date,
-        location: profile.location,
-      },
-      accountOverview: {
-        full_name: profile.full_name,
-        count_pass_course: profile.count_pass_course,
-        department: profile.department,
-        call_sign: profile.call_sign,
-        photo: profile.photo,
-      },
-    };
+    return transformProfileData(profile);
   }
 );
